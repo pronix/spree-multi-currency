6f32044c233acd7934df23997ebd40e34002066c
Maxim <parallel588@gmail.com>
2011-01-17 23:40:54 +0500

    added gem money

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,52 +1,68 @@
+require "money"
 class Currency < ActiveRecord::Base
-  has_many :currency_converters
+
+  has_many :currency_converters do
+    def get_rate(date)
+      last(:conditions => ["date_req <= ?", date])
+    end
+  end
+
+  # cattr_accessor :current
+  scope :locale, lambda{|str| where("locale like ?", "%#{str}%")}
 
   def basic!
-    update_attribute(:basic, true)
+    self.class.update_all(:basic => false) && update_attribute(:basic, true)
   end
+
   class << self
-    # Текущая валюта определенная по текущей локали
-    def current(_locale=I18n.locale)
-      find_by_locale(_locale.to_s)
+
+    def load_rate(options= {})
+      @_locale    = options[:locale]  || I18n.locale
+      @_date_rate = options[:date]    || Time.now
+      @basic      ||= basic
+      @current    ||= locale(@_locale).first
+
+      unless Money.default_bank.get_rate(@basic.char_code, @current.char_code)
+        if @rate = @current.currency_converters.get_rate(@_date_rate)
+          Money.add_rate(@basic.char_code, @current.char_code, @rate.nominal/@rate.value.to_f )
+        end
+      end
+
+      unless Money.default_bank.get_rate(@current.char_code, @basic.char_code)
+        if @rate = @current.currency_converters.get_rate(@_date_rate)
+          Money.add_rate(@current.char_code, @basic.char_code, @rate.value.to_f )
+        end
+      end
+
+    end
+
+    def convert(value, from, to)
+      Money.new(value.to_f * 100, from).exchange_to(to).to_f
     end
 
     # Конвертируем сумму к валюте текущей локале
     # Если валюта или локаль не найдена то возвращается та же сумма
     # Money.new(value.to_f, "Основаня").exchange_to("К Текущей").to_f
-    #
+    #Currency.conversion_to_current(100, :locale => "ru")
     def conversion_to_current(value, options = { })
-      if current_currency = check_current_currency(value, options)
-        (value.to_f / current_currency.value.to_f) * current_currency.nominal.to_i
-      else
-        value
-      end
+      load_rate(options)
+      convert(value, @basic.char_code, @current.char_code)
     end
 
     # Конвертируем значение из валюты текущей локали к основной валюте
     # в параметрах можно указать локаль из которой можно делать конвертацияю :locale
     # и дату курса :date, курс берется последний найденный до указанной даты
-    # Money.new(value.to_f, "Текущая локаль").exchange_to("К Основной").to_f
-    #
+    #Currency.conversion_from_current(100, :locale => "ru")
     def conversion_from_current(value, options={})
-      if current_currency = check_current_currency(value, options)
-        (value.to_f / current_currency.nominal.to_f ) * current_currency.value
-      else
-        value
-      end
+      load_rate(options)
+      convert(value,  @current.char_code, @basic.char_code)
     end
 
-
-    def check_current_currency(value, options)
-      return nil if value.to_f <= 0.0
-      @locale, @date = (options[:locale] || I18n.locale), (options[:date] || Time.now)
-      return nil if current(@locale).nil? || current(@locale).basic?
-      current_currency = current(@locale).currency_converters.last(:conditions => ["date_req <= ?", @date])
-      current_currency.blank? ? nil : current_currency
-    end
 
     # Основная валюта
     def basic
-      first(:conditions => { :basic => true })
+      @basic ||= first(:conditions => { :basic => true })
+      @basic
     end
 
     def get(num_code, options ={ })
--- multi_currencies.gemspec
+++ multi_currencies.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'multi_currencies'
-  s.version     = '1.0.0'
+  s.version     = '1.0.1'
   s.summary     = 'Add gem summary here'
   #s.description = 'Add (optional) gem description here'
   s.required_ruby_version = '>= 1.8.7'
 
-  # s.author            = 'David Heinemeier Hansson'
-  # s.email             = 'david@loudthinking.com'
-  # s.homepage          = 'http://www.rubyonrails.org'
-  # s.rubyforge_project = 'actionmailer'
+  # s.author            = ''
+  # s.email             = ''
+  # s.homepage          = ''
+  # s.rubyforge_project = ''
 
   s.files        = Dir['CHANGELOG', 'README.md', 'LICENSE', 'lib/**/*', 'app/**/*']
   s.require_path = 'lib'
@@ -18,5 +18,6 @@ Gem::Specification.new do |s|
   s.has_rdoc = true
 
   s.add_dependency('spree_core', '>= 0.30.0')
-  s.add_dependency('nokogiri', '>= 1.4.3.1')
+  s.add_dependency('nokogiri',   '>= 1.4.3.1')
+  s.add_dependency('money',      '>= 3.5.4')
 end
