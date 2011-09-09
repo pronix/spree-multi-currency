61c79df9c67c08b4c583377693dda801559484e5
Slava Mikerin <mikerin.slava@gmail.com>
2011-09-08 17:28:16 -0700

    updated hooks to overrides for admin menu

---
+++ app/overrides/add_currencies_admin_configurations_menu.rb
@@ -0,0 +1,17 @@
+Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :name => "currencies_admin_configurations_menu",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :disabled => false,
+                     :text => "
+      <% if current_user.has_role?(:admin) %>
+       <tr>
+         <td><%= link_to t(\"currency_settings\"), admin_currencies_path %></td>
+         <td><%= t(\"currency_description\") %></td>
+       </tr>
+       <tr>
+         <td><%= link_to t(\"currency_converters_settings\"), admin_currency_converters_path %></td>
+         <td><%= t(\"currency_converters_description\") %></td>
+       </tr>
+
+      <% end %>
+     ")
--- lib/multi_currencies.rb
+++ lib/multi_currencies.rb
@@ -3,12 +3,17 @@ require 'multi_currencies_hooks'
 
 module MultiCurrencies
   class Engine < Rails::Engine
+    railtie_name "spree-multi-currencies"
 
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
+      end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
 
