e3661870aa5246891d2d94bfd3f29aeb05d4a368
Martin Jesper Low Madsen <martin_jlowm@hotmail.com>
2011-12-15 00:57:26 +0100

    per_page is not available.

--- app/models/currency_converter.rb
+++ app/models/currency_converter.rb
@@ -1,7 +1,7 @@
 class CurrencyConverter < ActiveRecord::Base
   belongs_to :currency
   default_scope :order =>  "currency_converters.date_req ASC"
-  self.per_page = 15
+  
   class << self
     def add(currency, date, value, nominal)
       create({ :nominal => nominal, :value => value, :date_req => date, :currency => currency})
