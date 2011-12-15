bd074fa36c755b2b94e4a6d4171b3059d18d66c6
Martin Jesper Low Madsen <martin_jlowm@hotmail.com>
2011-12-15 19:10:49 +0100

    debugger.. DOH

--- lib/multi_currency.rb
+++ lib/multi_currency.rb
@@ -9,13 +9,13 @@ module MultiCurrency
   #  rate_at_date - использовать курс валюты на дату
   def multi_currency(*args)
     options = args.extract_options!
-    debugger
     [args].flatten.compact.each do |number_field|
-
       define_method(number_field.to_sym) do
         if options.has_key?(:rate_at_date) && options[:rate_at_date].is_a?(Proc)
-          Currency.conversion_to_current(read_attribute(number_field.to_sym),
-                                         { :date => options[:rate_at_date].call(self) })
+          Currency.conversion_to_current(
+          	read_attribute(number_field.to_sym),
+          	{ :date => options[:rate_at_date].call(self) }
+          )
         else
           Currency.conversion_to_current(read_attribute(number_field.to_sym))
         end
