9d7cd4a70c0d317b4c861370fcba13b98ecf620d
William Alton <bill@lovethe88.com>
2013-03-20 14:57:31 -0400

    Updating admin menu override for 2.0

--- app/overrides/add_currencies_admin_configurations_menu.rb
+++ app/overrides/add_currencies_admin_configurations_menu.rb
@@ -1,16 +1,10 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "currencies_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
+                     :insert_bottom => "ul[data-hook='admin_configurations_sidebar_menu']",
                      :disabled => false,
                      :text => "
-<% if current_user.has_role?(:admin) %>
-	<tr>
-		<td><%= link_to t('currency_settings'), admin_currencies_path %></td>
-		<td><%= t('currency_description') %></td>
-	</tr>
-	<tr>
-		<td><%= link_to t('currency_converters_settings'), admin_currency_converters_path %></td>
-		<td><%= t('currency_converters_description') %></td>
-	</tr>
+<% if current_user.has_spree_role?(:admin) %>
+  <%= configurations_sidebar_menu_item t(:currency_settings), admin_currencies_path %>
+  <%= configurations_sidebar_menu_item t(:currency_converters_settings), admin_currency_converters_path %>
 <% end %>
 ")
