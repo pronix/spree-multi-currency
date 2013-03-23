f1bbc67693532c9fffd63a80feb9eeb135acd7c2
dima <pronix.service@gmail.com>
2013-03-23 15:42:55 +0100

    update test

--- spec/changing_currency_spec.rb
+++ spec/changing_currency_spec.rb
@@ -6,13 +6,13 @@ describe "Currencies changing" do
     rub = Spree::Currency.create(:name => "rubles", :char_code => "RUB", :num_code => 623, :locale => "ru", :basic => false)
     usd = Spree::Currency.create(:name => "dollars", :char_code => "USD", :num_code => 624, :locale => "en", :basic => true)
     Spree::CurrencyConverter.create(:nominal => 32, :value => 1.0, :currency => rub, :date_req => Time.now)
-    product = FactoryGirl.create(:product, :price => 1)
-    variant = Spree::Variant.last
+    product = FactoryGirl.create(:product, :cost_price => 1)
+    variant = product.master
     I18n.locale = "en"
-    variant.price.should eql 1.0
+    variant.cost_price.should eql 1.0
     I18n.locale = "ru"
     Spree::Currency.current!
     Spree::Currency.current.should eql rub
-    variant.price.to_f.should eql 32.0
-  end  
+    variant.cost_price.to_f.should eql 32.0
+  end
 end
