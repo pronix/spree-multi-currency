58558ee0775c9e3c2ee0e270cb7d8ad69e735b05
dima <pronix.service@gmail.com>
2013-08-11 16:11:29 +0200

    add test for currency

---
+++ spec/models/spree/currency_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Spree::Currency do
+  before :each do
+    @rub = Spree::Currency.create(name: 'rubles', char_code: 'RUB',
+                                 num_code: 623, locale: 'ru', basic: false)
+  end
+
+  it 'set basic' do
+    @rub.basic!
+    @rub.basic.should be_true
+  end
+
+  it 'should return locale without spliting' do
+    @rub.locale = 'en,ru'
+    @rub.locale.should == ['en','ru']
+    @rub.locale(false).should == 'en,ru'
+  end
+
+  it 'should write error message' do
+    ex = Exception.new
+    ex.set_backtrace(['1','2'])
+    mess = " [ Currency ] :#{ex.inspect} \n #{ex.backtrace.join('\n ')}"
+    Rails.logger.should_receive(:error).with(mess)
+    Spree::Currency.error_logger(ex)
+  end
+
+end
