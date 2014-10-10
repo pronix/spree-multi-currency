944c3fe28207e159aee83838f3a582c735f6f208
Arthur CHAN <arthur@talkgoal.com>
2014-10-10 16:48:13 +0800

    2-4-stable should have updated the related decorated methods; and the methods seems not relate to currency

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,11 +2,7 @@
 
 Spree::Product.class_eval do
 
-   def variants_and_option_values(current_currency = nil)
-     variants.includes(:option_values).select do |variant|
-       variant.option_values.any?
-     end
-   end
+   
 
   # Can't use add_search_scope for this as it needs a default argument
   def self.available(available_on = nil, currency = nil)
@@ -21,19 +17,5 @@ Spree::Product.class_eval do
   # FIXME may be not require remove it from array
   search_scopes.delete(:available)
   search_scopes << :available
-end
-
-Spree::Core::Search::Base.class_eval do
-    def retrieve_products
-      @products_scope = get_base_scope
-      curr_page = page || 1
-
-      @products = @products_scope.includes([:master => :prices])
-      unless Spree::Config.show_products_without_price
-        @products = @products.where("spree_prices.amount IS NOT NULL")
-      end
-      @products = @products.page(curr_page).per(per_page)
-    end
-
 end
 
